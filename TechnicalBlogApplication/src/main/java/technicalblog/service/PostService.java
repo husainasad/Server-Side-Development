9f28af59dd59f4875219646aeffd18e87cf1c5ba
husainasad <shusain_be16@thapar.edu>
2020-02-10 22:58:01 +0530

    connection with JDBC

--- TechnicalBlogApplication/src/main/java/technicalblog/service/PostService.java
+++ TechnicalBlogApplication/src/main/java/technicalblog/service/PostService.java
@@ -3,6 +3,7 @@ package technicalblog.service;
 import org.springframework.stereotype.Service;
 import technicalblog.model.Post;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Date;
 @Service
@@ -13,38 +14,79 @@ public class PostService {
     public ArrayList<Post> getAllPosts() {
         ArrayList<Post> posts = new ArrayList<>();
 
-        Post post1 =  new Post();
-        post1.setTitle("Post 1");
-        post1.setBody("Post body 1");
-        post1.setDate(new Date());
-
-        Post post2 =  new Post();
-        post2.setTitle("Post 2");
-        post2.setBody("Post body 2");
-        post2.setDate(new Date());
-
-        Post post3 =  new Post();
-        post3.setTitle("Post 3");
-        post3.setBody("Post body 3");
-        post3.setDate(new Date());
-
-        posts.add(post1);
-        posts.add(post2);
-        posts.add(post3);
-
+//        Post post1 =  new Post();
+//        post1.setTitle("Post 1");
+//        post1.setBody("Post body 1");
+//        post1.setDate(new Date());
+//
+//        Post post2 =  new Post();
+//        post2.setTitle("Post 2");
+//        post2.setBody("Post body 2");
+//        post2.setDate(new Date());
+//
+//        Post post3 =  new Post();
+//        post3.setTitle("Post 3");
+//        post3.setBody("Post body 3");
+//        post3.setDate(new Date());
+//
+//        posts.add(post1);
+//        posts.add(post2);
+//        posts.add(post3);
+        Connection connection = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/technicalblog", "postgres", "Pass@123");
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM posts");
+            while(rs.next()){
+                Post post = new Post();
+                post.setTitle(rs.getString("title"));
+                post.setBody(rs.getString("body"));
+                posts.add(post);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        finally{
+            try{
+                connection.close();
+            }catch(SQLException e){
+                e.printStackTrace();
+            }
+        }
         return posts;
     }
     public ArrayList<Post> getOnePost() {
         ArrayList<Post> posts = new ArrayList<>();
 
-        Post post1 = new Post();
-        post1.setTitle("This is your Post");
-        post1.setBody("This is your Post. It has some valid content");
-        post1.setDate(new Date());
-        posts.add(post1);
-
+//        Post post1 = new Post();
+//        post1.setTitle("This is your Post");
+//        post1.setBody("This is your Post. It has some valid content");
+//        post1.setDate(new Date());
+//        posts.add(post1);
+        Connection connection = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/technicalblog", "postgres", "Pass@123");
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM posts where id = 4");
+            while(rs.next()){
+                Post post = new Post();
+                post.setTitle(rs.getString("title"));
+                post.setBody(rs.getString("body"));
+                posts.add(post);
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        finally{
+            try{
+                connection.close();
+            }catch(SQLException e){
+                e.printStackTrace();
+            }
+        }
         return posts;
-
     }
     public void createPost (Post newPost){
     }
